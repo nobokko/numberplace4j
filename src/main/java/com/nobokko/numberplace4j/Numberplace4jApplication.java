29b0aaef3d271afcd7086647deab1d7cd7b824bd
nobokko <nobokko+github@gmail.com>
2021-05-10 10:15:19 +0900

    VSCode : spring.initializr.maven-project

---
+++ src/main/java/com/nobokko/numberplace4j/Numberplace4jApplication.java
@@ -0,0 +1,13 @@
+package com.nobokko.numberplace4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Numberplace4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Numberplace4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nobokko/numberplace4j/Numberplace4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nobokko.numberplace4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Numberplace4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
